fa55cf5500c25be0e40c91f0bacb845948104693
Amanda Daly <techdaly@gmail.com>
2016-06-16 10:16:59 -0700

    Started adding answer data

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      answers: this.store.findAll('answer'),
+      questions: this.store.findAll('question'),
+    });
   },
-
+  
   actions: {
     saveq(qparams) {
       var newQuestion = this.store.createRecord('question', qparams);
